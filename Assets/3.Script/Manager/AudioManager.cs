94304adbd75f34190873c4d8eea94b402d643732
천사관영 <janyouns@gmail.com>
2024-03-14 04:58:01 +0900

    [Feat] MainGameScene

--- Assets/3.Script/Manager/AudioManager.cs
+++ Assets/3.Script/Manager/AudioManager.cs
@@ -6,6 +6,10 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance = null;
 
+    public AudioClip[] _bgmClips;
+
+    public AudioClip[] _sfxClips;
+
     private void Awake()
     {
         if(instance == null)
