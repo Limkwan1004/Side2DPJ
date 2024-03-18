f4a4994d09d540d87d28f4acaf4a083841ab7905
Limkwan1004 <janyouns@gmail.com>
2024-03-18 14:14:27 +0900

    [Feat] PlayerData

--- Assets/3.Script/Manager/GameManager.cs
+++ Assets/3.Script/Manager/GameManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class GameData
+public struct GameData
 {
-
+    int Level;
+    int Strength;
+    int Dexterity;
+    int Intelligence;
+    int Luck;
+    int SkillPoint;
+    int Exprience;
 }
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
+
+    public GameData _gameData = new GameData();
 
     private void Awake()
     {
