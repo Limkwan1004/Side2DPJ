30ad4eeb59e927df76fc38a15b8e6000cc5529cd
Limkwan1004 <janyouns@gmail.com>
2024-03-26 16:42:13 +0900

    [Fix] UnitData

--- Assets/3.Script/Unit/UnitData.cs
+++ Assets/3.Script/Unit/UnitData.cs
@@ -10,19 +10,31 @@ public class UnitData : ScriptableObject
     [SerializeField] private string _unitName;
     public string UnitName { get { return _unitName; } }
 
-    // 공격력
-    [SerializeField] private float _damage;
-    public float Damage { get { return _damage; } }
+    // 물리 공격력
+    [SerializeField] private float _ad;
+    public float AD { get { return _ad; } }
 
-    // 최대체력
-    [SerializeField] private float _maxHP;
-    public float MaxHP { get { return _maxHP; } }
+    // 마법 공격력
+    [SerializeField] private float _ap;
+    public float AP { get { return _ap; } }
+
+    // 공격속도
+    [SerializeField] private float _as;
+    public float AS { get { return _as; } }
+
+    // 방어력
+    [SerializeField] private float _df;
+    public float DF { get { return _df; } }
+
+    // 체력
+    [SerializeField] private float _hp;
+    public float HP { get { return _hp; } }
+
+    // 마나
+    [SerializeField] private float _mp;
+    public float MP { get { return _mp; } }
 
     // 이속
-    [SerializeField] private float _moveSpeed;
-    public float MoveSpeed { get { return _moveSpeed; } }
-
-    // 점프력
-    [SerializeField] private float _jumpForce;
-    public float JumpForce { get { return _jumpForce; } }
+    [SerializeField] private float _sp;
+    public float SP { get { return _sp; } }
 }
