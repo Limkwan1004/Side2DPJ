e14c9430790e76154d55bc3a7da6cb5f1644c752
임관영 <janyouns@gmail.com>
2024-06-21 12:49:06 +0900

    [Fix] GameData

--- Assets/3.Script/Manager/GameManager.cs
+++ Assets/3.Script/Manager/GameManager.cs
@@ -3,16 +3,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public struct GameData
-{
-
-}
-
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
-
-    public GameData _GameData = new GameData();
 
     private void Awake()
     {
